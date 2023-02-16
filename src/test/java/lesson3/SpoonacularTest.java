acae0ba73cb15442fd73d9e51e5fa240ae4cf68a
cosmoVoice <julia6796@mail.ru>
2023-02-16 11:52:46 +0300

    new

--- src/test/java/lesson3/SpoonacularTest.java
+++ src/test/java/lesson3/SpoonacularTest.java
@@ -1,2 +1,156 @@
-package lesson3;public class SpoonacularTest {
+package lesson3;
+import io.restassured.http.ContentType;
+import io.restassured.http.Cookie;
+import io.restassured.http.Headers;
+import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.lessThan;
+
+public class SpoonacularTest  extends AbstractTest{
+    @Test
+    void getSpecifyingRequestDataTest() {
+        //get
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("query", "pizza")
+                .queryParam("diet", "vegeterian")
+                .queryParam("number", "3")
+                .pathParam("id", 680975)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("cuisine", "Korean")
+                .queryParam("titleMatch", "kimchi")
+                .queryParam("number", "1")
+                .pathParam("id", 665379)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("maxProtein", "500")
+                .queryParam("includeIngredients", "lamb")
+                .pathParam("id", 660290)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("instructionsRequired", "true")
+                .queryParam("query", "soup")
+                .pathParam("id", 655055)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .queryParam("maxIron", "40")
+                .queryParam("cuisine", "European")
+                .pathParam("id", 715495)
+                .when()
+                .get(getBaseUrl()+"recipes/{id}/information")
+                .then()
+                .statusCode(200);
+        //post
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Cauliflower, Brown Rice, and Vegetable Fried Rice")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Turkey Tomato Cheese Pizza")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Winter Kimchi")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Apple Pie Smoothie")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+
+        given()
+                .queryParam("apiKey", getApiKey())
+                .contentType("application/x-www-form-urlencoded")
+                .formParam("title","Slow Cooker Lamb Curry")
+                .when()
+                .post(getBaseUrl()+"recipes/cuisine")
+                .then()
+                .statusCode(200);
+}
+
+    String id;
+    @Test
+    void addMealTest() {
+        id = given()
+                .queryParam("hash", "a3da66460bfb7e62ea1c96cfa0b7a634a346ccbf")
+                .queryParam("apiKey", "c5a3c0c99ec647718d6a6b711bd9b103")
+                .body("{\n"
+                        + " \"date\": 1644881179,\n"
+                        + " \"slot\": 2,\n"
+                        + " \"position\": 0,\n"
+                        + " \"type\": \"RECIPE\",\n"
+                        + " \"value\": {\n"
+                        + " \"id\": 296213,\"\n"
+                        + " \"servings\": 1,\"\n"
+                        + " \"title\": Pizza 73 BBQ Steak Pizza, 9,\"\n"
+                        + " \"imageType\"\": jpg\"\n"
+                        + " }\n"
+                        + "}")
+                .when()
+                .post("https://api.spoonacular.com/mealplanner/geekbrains/items")
+                .then()
+                .statusCode(200)
+                .extract()
+                .jsonPath()
+                .get("id")
+                .toString();
+    }
+    @AfterEach
+    void tearDown() {
+        given()
+                .queryParam("hash", "a3da66460bfb7e62ea1c96cfa0b7a634a346ccbf")
+                .queryParam("apiKey", "c5a3c0c99ec647718d6a6b711bd9b103")
+                .delete("https://api.spoonacular.com/mealplanner/geekbrains/items/" + id)
+                .then()
+                .statusCode(200);
+    }
 }
